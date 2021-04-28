082947420e8edb5aa8ec1318d0f9072e406ff460
Iván <dequin@hey.com>
2021-04-27 21:13:48 -0400

    Remove sample code

--- GoodNewsRxMVVM/NewsList/ViewModels/ViewModel.swift
+++ GoodNewsRxMVVM/NewsList/ViewModels/ViewModel.swift
@@ -3,23 +3,6 @@ import RxSwift
 class ViewModel: AlertPresentableViewModel {
     var alertModel: PublishSubject<AlertModel?> = PublishSubject<AlertModel?>()
     
-    func setup() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.alertModel.onNext(
-                AlertModel(
-                    actionModels: [
-                        AlertModel.ActionModel.init(
-                            title: "OK",
-                            style: .default,
-                            handler: nil)],
-                    title: "Alert example",
-                    message: "That's our easy alert.",
-                    prefferedStyle: .alert
-                )
-            )
-        }
-    }
-    
     func showOkAlert(message: String) {
         let alert = AlertModel(
             actionModels: [
